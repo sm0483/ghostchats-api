a2ce5003d47c987ac84f3def9a204b7c64577b99
sreerag <msratofficial@gmail.com>
2023-07-21 20:46:50 +0530

    fix:cors

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ const express = require("express");
 const app = express();
 
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
 const SocketEventHandler = require("./event/event-handler");
 const cors = require("cors");
 const idRouter = require("./router/router");
+
+const io = require("socket.io")(http, {
+   cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      credentials: true,
+   },
+});
 
 const pageNotFound = require("./middleware/not-found");
 const morgan = require("morgan");
